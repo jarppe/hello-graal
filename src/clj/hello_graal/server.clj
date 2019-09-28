f77efb15325daee06f9f95c2c3a5a562a9550023
Jarppe <jarppe@metosin.fi>
2019-09-28 12:47:11 +0300

    Add logging to show start times.

--- src/clj/hello_graal/server.clj
+++ src/clj/hello_graal/server.clj
@@ -1,5 +1,7 @@
 (ns hello-graal.server
   (:require [org.httpkit.server :as server])
+  (:import (java.time LocalDateTime)
+           (java.time.format DateTimeFormatter))
   (:gen-class))
 
 
@@ -9,5 +11,16 @@
    :body    "Hello!\n"})
 
 
+(defn log [message]
+  (->> (str (.format DateTimeFormatter/ISO_LOCAL_DATE_TIME (LocalDateTime/now)) " : " message)
+       (.println System/err)))
+
+
 (defn -main [& args]
-  (server/run-server handler {:port 8080}))
+  (log "server starting...")
+  (server/run-server handler {:port 8080})
+  (log "server ready"))
+
+
+(comment
+  (-main []))
